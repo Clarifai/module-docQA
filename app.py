354007ad28fccd841c29a9af44caa3c9b2878186
Minhajul Hoque <minh.hoque@clarifai.com>
2023-05-05 14:38:09 -0400

    updated main app documentation

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ st.sidebar.info("Select a page above.")
 
 st.markdown(
     """
-    **Helper module that allows you to upload and explore large amount of texts** 👈 
-    ### Pages
-    - Investigate: Use this page to explore the texts you uploaded to a Clarifai application. Find similar texts and continue your investigation.
-    - Upload: Upload and chunk a PDF to a Clarifai application.
+    **Helper module that allows you to upload and explore large amount of texts**
+    ## 📄 Pages
+    #### 🌐 Geo Search
+    Use this page to find explore texts that are related to a location.
+    #### 🔍 Investigate
+    Use this page to explore the texts you uploaded to a Clarifai application. Find similar texts and continue your investigation.
+    #### 📟 Upload
+    Upload and chunk a PDF to a Clarifai application.
+    #### 📟 🌐 Upload with geo
+    Upload and chunk with geo data a PDF to a Clarifai application.
 """
 )
