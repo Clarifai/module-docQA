ac4bcda4d8cca6bd636114b960dae87cd5a4cb3c
mogith-pn <143642606+mogith-pn@users.noreply.github.com>
2023-09-26 17:40:48 +0530

    updated client.auth import

--- pages/investigate.py
+++ pages/investigate.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import pandas as pd
 import streamlit as st
-from clarifai.auth.helper import ClarifaiAuthHelper
+from clarifai.client.auth.helper import ClarifaiAuthHelper
 from clarifai.client.auth import create_stub
 from clarifai.modules.css import ClarifaiStreamlitCSS
 from clarifai_grpc.grpc.api import service_pb2
